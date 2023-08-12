f9d24a66268a58155c9b8c346ecd80e97722db3f
ChistilinMV <max.chistilin@gmail.com>
2023-08-12 23:08:15 +0300

    some change in HomeWork02

--- src/main/java/homework02/Main.java
+++ src/main/java/homework02/Main.java
@@ -3,11 +3,9 @@ package homework02;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
-import java.util.concurrent.ThreadLocalRandom;
 
 public class Main {
     public static void main(String[] args) {
-        Random rnd = ThreadLocalRandom.current();
         List<ItemFabric> fabricList = new ArrayList<>();
 
         fabricList.add(new GoldGenerator());
