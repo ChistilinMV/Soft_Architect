bf99940922a7be5e7c15cd7381ce982eb0adf536
ChistilinMV <max.chistilin@gmail.com>
2023-09-08 00:59:43 +0300

    HomeWork07

--- src/main/java/homework07/erd/report_service_erd.sql
+++ src/main/java/homework07/erd/report_service_erd.sql
@@ -1,59 +1,93 @@
-CREATE TABLE Provider (
-  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  name TEXT NOT NULL,
-  adress TEXT NOT NULL,
-  idProviderProduct INT NOT NULL,
-  FOREIGN KEY (idProviderProduct) REFERENCES ProviderProduct(id)
-);
+/*
+ Navicat Premium Data Transfer
 
-CREATE TABLE Customer (
-  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  name TEXT NOT NULL,
-  adress TEXT NOT NULL
-);
+ Source Server         : system4
+ Source Server Type    : MariaDB
+ Source Server Version : 101002
+ Source Host           : 192.168.2.100:3306
+ Source Schema         : lesson07
 
-CREATE TABLE ProviderProduct (
-  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  idProduct INT NOT NULL,
-  price FLOAT NOT NULL,
-  amount INT NOT NULL,
-  FOREIGN KEY (idProduct) REFERENCES Prodict(id)
-);
+ Target Server Type    : MariaDB
+ Target Server Version : 101002
+ File Encoding         : 65001
 
-CREATE TABLE LogBook (
-  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  idProduct INT NOT NULL,
-  idProvider INT NOT NULL,
-  idCustomer INT NOT NULL,
-  count INT NOT NULL,
-  price FLOAT NOT NULL,
-  FOREIGN KEY (idProduct) REFERENCES Prodict(id),
-  FOREIGN KEY (idProvider) REFERENCES Provider(id),
-  FOREIGN KEY (idCustomer) REFERENCES Customer(id)
-);
+ Date: 07/09/2023 17:58:56
+*/
 
-CREATE TABLE Prodict (
-  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  name TEXT NOT NULL,
-  idCategory INT NOT NULL,
-  isSubCategory INT NOT NULL,
-  idType INT NOT NULL,
-  FOREIGN KEY (idCategory) REFERENCES Category(id),
-  FOREIGN KEY (isSubCategory) REFERENCES SubCategory(id),
-  FOREIGN KEY (idType) REFERENCES TypeProduct(id)
-);
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
 
-CREATE TABLE Category (
-  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  name TEXT NOT NULL
-);
+-- ----------------------------
+-- Table structure for Customers
+-- ----------------------------
+DROP TABLE IF EXISTS `Customers`;
+CREATE TABLE `Customers` (
+  `customer_id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `address` varchar(255) DEFAULT NULL,
+  `phone_number` varchar(20) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT current_timestamp(),
+  PRIMARY KEY (`customer_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_general_ci;
 
-CREATE TABLE SubCategory (
-  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  name TEXT NOT NULL
-);
+-- ----------------------------
+-- Table structure for OrderItems
+-- ----------------------------
+DROP TABLE IF EXISTS `OrderItems`;
+CREATE TABLE `OrderItems` (
+  `order_item_id` int(11) NOT NULL AUTO_INCREMENT,
+  `order_id` int(11) DEFAULT NULL,
+  `product_id` int(11) DEFAULT NULL,
+  `quantity` int(11) DEFAULT NULL,
+  `price` decimal(10,2) DEFAULT NULL,
+  PRIMARY KEY (`order_item_id`),
+  KEY `order_id` (`order_id`),
+  KEY `product_id` (`product_id`),
+  CONSTRAINT `OrderItems_ibfk_1` FOREIGN KEY (`order_id`) REFERENCES `Orders` (`order_id`),
+  CONSTRAINT `OrderItems_ibfk_2` FOREIGN KEY (`product_id`) REFERENCES `Products` (`product_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_general_ci;
 
-CREATE TABLE TypeProduct (
-  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  name TEXT NOT NULL
-);
+-- ----------------------------
+-- Table structure for Orders
+-- ----------------------------
+DROP TABLE IF EXISTS `Orders`;
+CREATE TABLE `Orders` (
+  `order_id` int(11) NOT NULL AUTO_INCREMENT,
+  `customer_id` int(11) DEFAULT NULL,
+  `order_date` date DEFAULT NULL,
+  `total_amount` decimal(10,2) DEFAULT NULL,
+  PRIMARY KEY (`order_id`),
+  KEY `customer_id` (`customer_id`),
+  CONSTRAINT `Orders_ibfk_1` FOREIGN KEY (`customer_id`) REFERENCES `Customers` (`customer_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_general_ci;
+
+-- ----------------------------
+-- Table structure for Products
+-- ----------------------------
+DROP TABLE IF EXISTS `Products`;
+CREATE TABLE `Products` (
+  `product_id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `price` decimal(10,2) DEFAULT NULL,
+  `supplier_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`product_id`),
+  KEY `supplier_id` (`supplier_id`),
+  CONSTRAINT `Products_ibfk_1` FOREIGN KEY (`supplier_id`) REFERENCES `Suppliers` (`supplier_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_general_ci;
+
+-- ----------------------------
+-- Table structure for Suppliers
+-- ----------------------------
+DROP TABLE IF EXISTS `Suppliers`;
+CREATE TABLE `Suppliers` (
+  `supplier_id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `address` varchar(255) DEFAULT NULL,
+  `phone_number` varchar(20) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT current_timestamp(),
+  PRIMARY KEY (`supplier_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_general_ci;
+
+SET FOREIGN_KEY_CHECKS = 1;
